789e91327b306a9ec1c1a6d4f68a046ec8c786a4
leob03 <leobr.1809@gmail.com>
2023-07-10 20:14:05 -0400

    update

--- scripts/LTD_main_test_error.py
+++ scripts/LTD_main_test_error.py
@@ -21,14 +21,12 @@ import torch.optim as optim
 from progress.bar import Bar
 import pandas as pd
 
-from utils.h36motion3d import Datasets
 from utils import loss_funcs, utils as utils
 import model.GCN as nnmodel
 from utils.opt import Options
 from utils import util
 from utils import log
 import utils.data_utils as data_utils
-from utils.h36motion3d import H36motion3D
 
 batch_size = 1
 num_frames = 20
@@ -47,7 +45,7 @@ script_name = os.path.basename(__file__).split('.')[0]
 script_name = script_name + '_3D_in{:d}_out{:d}_dct_n_{:d}'.format(opt.input_n, opt.output_n, opt.dct_n)
 
 print('>>> create models')
-nput_n = opt.input_n
+input_n = opt.input_n
 output_n = opt.output_n
 dct_n = opt.dct_n
 sample_rate = opt.sample_rate
@@ -79,6 +77,7 @@ def run_model(model, optimizer=None, is_train=0, input_data=None, epo=1, opt=Non
     n = 0
     in_n = opt.input_n
     out_n = opt.output_n
+    dct_used = in_n + out_n
     dim_used = np.array([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25,
                          26, 27, 28, 29, 30, 31, 32, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
                          46, 47, 51, 52, 53, 54, 55, 56, 57, 58, 59, 63, 64, 65, 66, 67, 68,
@@ -100,20 +99,28 @@ def run_model(model, optimizer=None, is_train=0, input_data=None, epo=1, opt=Non
     p3d_src = p3d_h36.clone()[:, :, dim_used]
 
     #First I need to transform p3d_src in dct coeficients before I give it to the predictor
+    dct_m_in, _ = data_utils.get_dct_matrix(in_n + out_n)
+    pad_idx = np.repeat([input_n - 1], output_n)
+    i_idx = np.append(np.arange(0, input_n), pad_idx)
+    input_dct_seq = np.matmul(dct_m_in[0:dct_used, :], p3d_src[i_idx, :])
+    input_dct_seq = input_dct_seq.transpose().reshape([-1, len(dim_used), dct_used])
+    # input_dct_seq = input_dct_seq.reshape(-1, len(dim_used) * dct_used)
 
-    p3d_out_all = model(p3d_src*1000)
+    #make sure dim_used in LTD is the same as in HRI
+
+    p3d_out_all = model(input_dct_seq*1000)
     p3d_out_all = p3d_out_all*0.001
 
     _, idct_m = data_utils.get_dct_matrix(seq_n)
     idct_m = torch.from_numpy(idct_m).float().cuda()
     outputs_t = p3d_out_all.view(-1, dct_n).transpose(0, 1)
     outputs_3d = torch.matmul(idct_m[:, 0:dct_n], outputs_t).transpose(0, 1).contiguous().view(-1, dim_used_len,seq_n).transpose(1,2)
+    
     pred_3d = p3d_h36.clone()
-
-
     pred_3d[:, :, dim_used] = outputs_3d
     pred_3d[:, :, index_to_ignore] = pred_3d[:, :, index_to_equal]
     pred_p3d = pred_3d.contiguous().view(n, seq_n, -1, 3)[:, in_n:, :, :]
+
     targ_p3d = p3d_h36.contiguous().view(n, seq_n, -1, 3)[:, in_n:, :, :]
 
     # p3d_out_all = p3d_out_all[:, seq_in:].transpose(1, 2).reshape([batch_size, 10 * itera, -1])[:, :out_n]
